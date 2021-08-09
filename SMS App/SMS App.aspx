c57b173a95b9ca2c749e655e0a1c5ebf9087c935
Tamuka Manjemu <tkmanjemu@gmail.com>
2021-08-09 11:56:13 +0200

    Add project files.

---
+++ SMS App/SMS App.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SMS App.aspx.cs" Inherits="SMS_App.SMS_App" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <form id="form1" runat="server">    
+         <div>    
+            <asp:Label runat="server" Text="Enter Mobile Number"></asp:Label>    
+            <asp:TextBox runat="server"></asp:TextBox>    
+            <br />    
+            <asp:Button runat="server" ID="btnSent" Text="Sent SMS" onClick="btnSent_Click"></asp:Button>    
+         </div>    
+      </form>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ SMS App/SMS App.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SMS_App
+{
+    public partial class SMS_App : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSent_Click(object sender, EventArgs e)
+        {
+            // use the API URL here  
+            string strUrl = "http://api.mVaayoo.com/mvaayooapi/MessageCompose?user=YourUserName:YourPassword&senderID=YourSenderID&    receipientno=1234567890&msgtxt=This is a test from mVaayoo API&state=4";
+            // Create a request object  
+            WebRequest request = HttpWebRequest.Create(strUrl);
+            // Get the response back  
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream s = (Stream)response.GetResponseStream();
+            StreamReader readStream = new StreamReader(s);
+            string dataString = readStream.ReadToEnd();
+            response.Close();
+            s.Close();
+            readStream.Close();
+        }
+    }
+    
+}
